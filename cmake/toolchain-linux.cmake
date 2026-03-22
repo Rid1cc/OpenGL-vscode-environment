8c958ebebb84018ddb66123eac00e890adc71bc6
razny <47929286+Rid1cc@users.noreply.github.com>
2026-03-22 21:09:14 +0100

    Delete cmake/toolchain-linux.cmake

--- cmake/toolchain-linux.cmake
+++
@@ -1,7 +0,0 @@
-# Linux Toolchain (GCC)
-#
-# This toolchain file is for native Linux GCC builds.
-# You can extend it if you want to use a custom compiler or sysroot.
-
-# No special settings needed for standard Linux GCC builds.
-# You can add custom variables or definitions here if needed.
